c95f382f5ceda00336431106059dccae4e92b0ad
viet.dinhduc <ducviet2504@gmail.com>
2021-03-20 13:05:04 +0700

    add node

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var app = express();
+var low = require('lowdb')
+var FileSync = require('lowdb/adapters/FileSync')
+var adapter = new FileSync('db.json')
+
+
 const port = 3001;
+
 app.set("view engine", "pug");
 app.set("views", "./views");
+const db = low(adapter)
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write()
 
-var users = [
-  { id: 1, name: "viet" },
-  { id: 2, name: "Hang" },
-];
+
+// var users = [
+//   { id: 1, name: "viet" },
+//   { id: 2, name: "Hang" },
+// ];
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -17,9 +28,15 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.render("users/index", {
-    users: users,
+    users: db.get('users').value(),
   });
 });
+
+app.get('/users/create', function(req, res){
+  res.render("users/create", {
+    users: db.get('users').value(),
+  });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -31,7 +48,5 @@ app.get("/users/search", (req, res) => {
     return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
 
-  res.render("users/index", {
-    users: matchedUsers,
-  });
+  
 });
